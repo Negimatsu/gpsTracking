326f19834444099c531310519ce06a46a423a6ae
negimatsu <ong.dkrab@gmail.com>
2013-11-09 05:52:05 +0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GpsTracking::Application.config.secret_key_base = '3ebad770eb46a2436c28ac1a863721e1b1174ca856bff740d8018401407232877dd37ac6b9b175987c780aece0570c3f6cac479577d81c5be0c7a1f9128b92ea'
