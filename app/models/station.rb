95f3d7340e9cd4bd41007852bd6ee344be3cb141
negimatsu <ong.dkrab@gmail.com>
2013-12-10 13:51:39 +0700

    update station show map

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,43 +1,53 @@
 class Station < ActiveRecord::Base
 
+  def in_station? px,py
+    x = [self.x1, self.x2, self.x3, self.x4]
+    y = [self.y1, self.y2, self.y3, self.y4]
+    tri = []
+    (0..3).each do |i|
+      l_x = [x[i-1], x[i]] + [px]
+      l_y = [y[i-1], y[i]] + [py]
+      tri = tri + [area_cal(l_x, l_y)]
+    end
+
+    sum_tri = sum_area_method(tri)
+    area_rec = area_cal(x,y)
+    diff = (area_rec - sum_tri).abs
+
+    return diff < 10**-10 #if in area is true
+
+  end
+
+
   def to_api
-    "markers=color:green%7Clabel:#{station}%7C#{lat},#{lng}"
+    "markers=color:green%7Clabel:#{(station+65).chr}%7C#{lat},#{lng}"
   end
 
   def to_p_maps
     self.to_api + "&"+
-    "markers=color:blue%7Clabel:#{station}%7C#{x1},#{y1}&" +
-    "markers=color:blue%7Clabel:#{station}%7C#{x2},#{y2}&" +
-    "markers=color:blue%7Clabel:#{station}%7C#{x3},#{y3}&" +
-    "markers=color:blue%7Clabel:#{station}%7C#{x4},#{y4}&"
+        "markers=color:blue%7Clabel:1%7C#{x1},#{y1}&" +
+        "markers=color:blue%7Clabel:2%7C#{x2},#{y2}&" +
+        "markers=color:blue%7Clabel:3%7C#{x3},#{y3}&" +
+        "markers=color:blue%7Clabel:4%7C#{x4},#{y4}&"
   end
 
-  def in_station? x1,y1,x2,y2,x3,y3,x4,y4,px,py
-    a1 = Math.sqrt(((x1-x2)**2 + (y1-y2)**2))
-    a2 = Math.sqrt(((x2-x3)**2 + (y2-y3)**2))
-    a3 = Math.sqrt(((x3-x4)**2 + (y3-y4)**2))
-    a4 = Math.sqrt(((x4-x1)**2 + (y4-y1)**2))
 
-    b1 = Math.sqrt(((x1-px)**2 + (y1-py)**2))
-    b2 = Math.sqrt(((x2-px)**2 + (y2-py)**2))
-    b3 = Math.sqrt(((x3-px)**2 + (y3-py)**2))
-    b4 = Math.sqrt(((x4-px)**2 + (y4-py)**2))
+private
 
-    u1 = (a1+b1+b2)/2
-    u2 = (a2+b2+b3)/2
-    u3 = (a3+b3+b4)/2
-    u4 = (a4+b4+b1)/2
-
-    p area1 = Math.sqrt(u1*(u1-a1)*(u1-b1)*(u1-b2))
-    p area2 = Math.sqrt(u2*(u2-a2)*(u2-b2)*(u2-b3))
-    p area3 = Math.sqrt(u3*(u3-a3)*(u3-b3)*(u3-b4))
-    p area4 = Math.sqrt(u4*(u4-a4)*(u4-b4)*(u4-b1))
-    p a1*a2
-    if a1*a2 == area1 + area2 + area3 + area4
-      return true
-    else
-      return false
+  def area_cal (x, y)
+    sum_area = 0
+    (1..(x.length-1)).each do |i|
+      sum_area = sum_area + (y[i-1]*x[i]) - (y[i]*x[i-1])
     end
+    sum_area = sum_area + (x[0]*y[-1])-(x[-1]*y[0])
+    area = (sum_area/2).abs
+
+    area
   end
+
+  def sum_area_method(area_list)
+    area_list.inject(:+)
+  end
+
 
 end
