b6a81c23c81fe72286effb1b936a9210d29eb795
negimatsu <ong.dkrab@gmail.com>
2014-01-26 23:06:06 +0700

    update find next station

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -72,6 +72,10 @@ class TrackingsController < ApplicationController
       before_index = before_id != 0 ? (ListStation.find_by_station_id(before_id)).list_number : 1
       before_before_index = before_id != 0 ? (ListStation.find_by_station_id(before_station before_index)).list_number : 1
       result = []
+      p "###############"
+      p cur_index
+      p before_before_index
+      p before_index
       if cur_index == 1
         result.push(ListStation.find_by_station_id(2).station_id)
 
